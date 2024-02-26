71be6612f195d541e2193a2f0221f87f5a1be50e
Emma Viitanen <viem23ni@student.ju.se>
2024-02-26 11:54:49 +0100

    small update

--- test.js
+++ test.js
@@ -5,11 +5,27 @@ let singlePlayerMode = true;
 
 function setup() {
   createCanvas(800, 600);
-  
+
   // Initialize players
-  player1 = new Car(color(0, 0, 255), width/2 - 50, height - 50, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW);
-  player2 = new Car(color(255, 255, 0), width/2 + 50, height - 50, 87, 83, 65, 68); // A, D, W, S
-  
+  player1 = new Car(
+    color(0, 0, 255),
+    width / 2 - 50,
+    height - 50,
+    UP_ARROW,
+    DOWN_ARROW,
+    LEFT_ARROW,
+    RIGHT_ARROW
+  );
+  player2 = new Car(
+    color(255, 255, 0),
+    width / 2 + 50,
+    height - 50,
+    87,
+    83,
+    65,
+    68
+  ); // A, D, W, S
+
   // Initialize track
   track = new Track();
   // Define track outline points
@@ -18,31 +34,35 @@ function setup() {
   track.addPoint(700, 500);
   track.addPoint(100, 500);
   track.addPoint(100, 100); // Close the loop
-  
+
   timeElapsed = 0;
 }
 
 function draw() {
   background(200);
-  
+
   // Draw track
   track.display();
-  
+
   // Draw cars
   player1.update();
   player1.display();
-  
+
   player2.update();
   player2.display();
-  
+
   // Display time
   displayTime();
-  
+
   // Display mode text
   fill(0);
   textSize(20);
   textAlign(CENTER, CENTER);
-  text(singlePlayerMode ? "Single Player Mode" : "Two Player Mode", width / 2, 20);
+  text(
+    singlePlayerMode ? "Single Player Mode" : "Two Player Mode",
+    width / 2,
+    20
+  );
 }
 
 function displayTime() {
@@ -67,13 +87,13 @@ class Car {
     this.width = 20;
     this.height = 40;
   }
-  
+
   update() {
     this.checkKeys();
     this.pos.add(this.vel);
     // Apply friction or other forces here
   }
-  
+
   checkKeys() {
     if (keyIsDown(this.upKey)) {
       this.acc.y = -0.1;
@@ -82,7 +102,7 @@ class Car {
     } else {
       this.acc.y = 0;
     }
-    
+
     if (keyIsDown(this.leftKey)) {
       this.acc.x = -0.1;
     } else if (keyIsDown(this.rightKey)) {
@@ -90,11 +110,11 @@ class Car {
     } else {
       this.acc.x = 0;
     }
-    
+
     this.vel.add(this.acc);
     this.vel.limit(this.maxSpeed);
   }
-  
+
   display() {
     fill(this.color);
     rectMode(CENTER);
@@ -106,11 +126,11 @@ class Track {
   constructor() {
     this.points = [];
   }
-  
+
   addPoint(x, y) {
     this.points.push(createVector(x, y));
   }
-  
+
   display() {
     stroke(0);
     strokeWeight(3);
@@ -121,6 +141,4 @@ class Track {
     }
     endShape(CLOSE);
   }
-} 
-
- 
+}
