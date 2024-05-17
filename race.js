e45643f8841f29e5d632dd76e53dd7acd8e3e0eb
Emma Viitanen <viem23ni@student.ju.se>
2024-05-17 12:08:54 +0200

    buttons fixed on web

--- race.js
+++ race.js
@@ -1,6 +1,5 @@
 import { RedCar } from "./traffic.js";
 import { PowerUp } from "./powerup.js";
-import { resetGame } from "./reset.js";
 import { PlayerCar } from "./playercar.js";
 import { FallingLine } from "./lines.js";
 
@@ -156,6 +155,44 @@ function setup() {
 window.setup = setup;
 
 //Player1 car coordinates
+
+function resetGame() {
+  // Reset player car position
+  singlePlayer.x = innerWidth / 2;
+  singlePlayer.y = 550;
+
+  // Reset traffic cars on the left side
+  for (let i = 0; i < numcars; i++) {
+    let x = random(130, 300);
+    let y = random(-500, 0) - i * spacing;
+    cars[i].x = x;
+    cars[i].y = y;
+    cars[i].scored = false; // Reset scored flag
+  }
+
+  // Reset traffic cars on the right side
+  for (let i = 0; i < numcars; i++) {
+    let x = random(300, 532);
+    let y = random(-500, 0) - i * spacing;
+    carsright[i].x = x;
+    carsright[i].y = y;
+    carsright[i].scored = false; // Reset scored flag
+  }
+
+  // Reset powerups
+  for (let i = 0; i < numpowerup; i++) {
+    let x = random(130, 532);
+    let y = random(-500, 0);
+    powerup[i].x = x;
+    powerup[i].y = y;
+  }
+
+  // Reset score and powerup state
+  score = 0;
+  powerupActive = false;
+  powerupTime = 0;
+}
+window.resetGame = resetGame;
 
 let playerCarY = 450;
 
@@ -352,10 +389,12 @@ function resultOneScreen() {
   // These 8 lines of code was adapted from https://pixelkind.github.io/foundationsofprogramming/oop/01-02-example. Accessed: 11/5-2024
   if (mouseIsPressed) {
     if (restartButton.hitTest(mouseX, mouseY)) {
+      console.log("reset button was clicked");
       resetGame();
       state = "onePlayer";
     }
     if (menuButton.hitTest(mouseX, mouseY)) {
+      console.log("menu button was clicked");
       resetGame();
       state = "start";
     }
